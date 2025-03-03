89fffc7b605fdd581600477422ef89cf0443345d
gotofurther <1914948029@qq.com>
2025-03-03 19:01:13 +0800

    2025/3/3-19:00

--- Cpp-STL/main.cpp
+++ Cpp-STL/main.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int dic[25];
+
+int main(){
+	vector<char> str;
+	char a;
+	while(scanf("%c", &a), str.push_back(a), a != '\n');
+	for(int i = 0; i < str.size(); i++){
+		dic[sttr[i] - 'a']++;
+	}
+	for(int i = 0; i < str.size(); i++){
+		
+	}
+}
